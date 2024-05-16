edadad7326db14fa3270f08a73ddc3e8ca01c4e9
Felipe <felipe.braga@igs-software.com.br>
2024-05-16 17:29:36 -0300

    Acrescentando a minha trajetória

--- js/scripts.js
+++ js/scripts.js
@@ -39,3 +39,30 @@ function createParticles(event) {
         }, 1000);
     }
 }
+
+// Interatividade da Timeline
+document.addEventListener('DOMContentLoaded', function () {
+    const items = document.querySelectorAll('.timeline-item');
+
+    const isInViewport = (el) => {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    const run = () =>
+        items.forEach((item) => {
+            if (isInViewport(item)) {
+                item.classList.add('in-view');
+            }
+        });
+
+    // Events
+    window.addEventListener('load', run);
+    window.addEventListener('resize', run);
+    window.addEventListener('scroll', run);
+});
